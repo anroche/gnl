c04268520585ad6c23f5e413eda23141cdf9e28a
Antoine ROCHE <anroche@e1z2r7p43.42.us.org>
2017-03-13 19:15:11 -0700

    test2

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: anroche <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/20 18:41:41 by anroche           #+#    #+#             */
-/*   Updated: 2017/03/11 18:04:52 by anroche          ###   ########.fr       */
+/*   Updated: 2017/03/13 19:14:44 by anroche          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,7 @@ char	*ft_check_line(int fd, char **tmp)
 	while ((buf = ft_strnew(BUFF_SIZE)) &&
 	read(fd, buf, BUFF_SIZE) &&
 	!ft_memchr(buf, '\n', BUFF_SIZE))
-	{	
 		ret = ft_strconcat(ret, buf);
-	}
-	
 	if (ft_memchr(buf, '\n', BUFF_SIZE) != NULL)
 		*tmp = ft_strcop(buf);
 	return (ret);
@@ -33,16 +30,22 @@ char	*ft_check_line(int fd, char **tmp)
 char	*ft_check_end_buffer(char **tmp)
 {
 	char	*ret;
+	char	*stock;
 	int		i;
 
+	stock = NULL;
 	i = 0;
 	ret = NULL;
 	if (tmp != NULL)
 	{
-		while ((*tmp)[i] != '\n')
+		while ((*tmp)[i] != '\n' && (*tmp)[i] != '\n')
 			i++;
-		ft_strcut(*tmp, &ret, tmp, i);
+		ft_strcut(*tmp, &ret, &stock, i);
 	}
+	*tmp = NULL;
+	*tmp = stock;
+	ft_putstr("\ntest ret |");
+	ft_putstr(ret);
 	return (ret);
 }
 
@@ -51,25 +54,24 @@ int	get_next_line(const int fd, char **line)
 	char			*buffer[BUFF_SIZE];
 	char			*ret;
 	static char		*tmp;
-
+	
 	ret = NULL;
 	if (read(fd, buffer, 0) == -1 || fd == -1 || !line)
 		 return (-1);
 	if (tmp)
 		ret = ft_strconcat(ret, ft_check_end_buffer(&tmp));
-	if (!(ft_memchr(tmp, '\n', ft_strlen(tmp))))
+	ft_putstr("|tmp|");
+	ft_putstr(tmp);
+	ft_putstr("|ret|");
+	ft_putstr(ret);
+	//	tmp = ;
+//	if (!(ft_memchr(tmp, '\n', ft_strlen(tmp))))
 	{
 		ret = ft_strconcat(ret, ft_check_line(fd, &tmp));
 		if (tmp && ft_memchr(tmp, '\n', BUFF_SIZE))
 			ret = ft_strconcat(ret, ft_check_end_buffer(&tmp));
-		ft_putstr("line1:");
-		ft_putstr(*line);
 		*line = ft_strcop(ret);
-		ft_putstr("\nline2:");
-		ft_putstr(*line);
 	}
-	ft_putstr("\ntest =");
-	ft_putstr(*line);
 
 	return (((*line)[0] != '\0') ? 1 : 0);
 }
